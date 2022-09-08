e3260ed0a254809ecb64ac65031f241bff2fe1b9
wintttr <85644131+wintttr@users.noreply.github.com>
2022-09-08 20:16:41 +0300

    Версия ещё чуть-чуть лучше

--- Lab_1/Source.cpp
+++ Lab_1/Source.cpp
@@ -5,6 +5,44 @@
 
 using namespace std;
 
+class mask {
+	int n;
+	string curMask;
+
+public:
+	mask(int _n) : n(_n) {
+		curMask = string(_n, 'f');
+	}
+
+	const string& getMask() const & {
+		return curMask;
+	}
+
+	string getMask() && {
+		return move(curMask);
+	}
+
+
+	bool nextExists() {
+		for (int i = 0; i < curMask.size(); i++)
+			if (curMask[i] == 't')
+				return true;
+		return false;
+	}
+
+	void next() {
+		for (int i = curMask.size() - 1; i >= 0; i--) {
+			if (curMask[i] == 'f') {
+				curMask[i] = 't';
+				break;
+			}
+			else
+				curMask[i] = 'f';
+		}
+	}
+};
+
+
 string inv(const string& s) {
 	string result;
 	for (int i = 0; i < s.size(); i++)
@@ -27,44 +65,17 @@ bool match(const string& mask, const vector<string>& v, const vector<int>& c) {
 	return true;
 }
 
-bool nextExists(const string& mask) {
-	if (mask == "tttttttttt")
-		return false;
-	else
-		return true;
-}
-
-string nextMask(const string& mask) {
-	string newMask(mask);
-
-	for (int i = newMask.size() - 1; i >= 0; i--) {
-		if (newMask[i] == 'f') {
-			newMask[i] = 't';
-			break;
-		}
-		else
-			newMask[i] = 'f';
-	}
-	
-	return newMask;
-}
-
 int main() {
+	// Начнём с ввода
 	int N;
-	string mask{ 10, 'f' };
-
-	string trueSolution;
-
-	vector<string> ans;
-	vector<int> solutionCount;
-
-	bool one = false;
 
 	ifstream fin("input.txt");
 	ofstream fout("output.txt");
 
-	fin >> N;
+	vector<string> ans; // Ответы, введённые игроком
+	vector<int> solutionCount; // Количество правильных ответов
 
+	fin >> N;
 	for (int i = 0; i < N; i++) {
 		string temp;
 		fin >> temp;
@@ -74,26 +85,38 @@ int main() {
 		ans.push_back((move(temp)).substr(0, 10));
 	}
 
-	for (int i = 0; i < N; i++)
+	mask m(10); 
+	string trueSolution;
+	
+	bool one = false;
+	bool found = false;
+
+	int solCnt = 0; // Количество масок, подходящих
+					// под условие
+					
+
+	// Ищем решение, в котором было 0 правильных ответов
+	// Если такое есть, то правильное решение - это данное
+	// решение наоборот, причём оно единственное
+	for (int i = 0; i < N; i++) 
 		if (solutionCount[i] == 0) {
 			trueSolution = move(inv(ans[i]));
-			one = true;
+			found = true;
+			solCnt = 1;
 		}
 
-	int solCnt = 0;
-	if (one != true)
-		while (nextExists(mask)) {
-			if (match(mask, ans, solutionCount)) {
+
+	if (!found)
+		do {
+			if (match(m.getMask(), ans, solutionCount)) {
 				solCnt++;
-				trueSolution = mask;
+				trueSolution = m.getMask();
 			}
-			mask = nextMask(mask);
-		}
 
-	if (solCnt == 1)
-		one = true;
+			m.next();
+		} while (m.nextExists());
 
-	fout << (one ? "ONE SOLUTION" : "POSSIBLE SOLUTION") << endl;
+	fout << (solCnt == 1 ? "ONE SOLUTION" : "POSSIBLE SOLUTION") << endl;
 	fout << trueSolution << endl;
 	
 	fin.close();
